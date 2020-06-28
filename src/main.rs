e9abef8a655e9f5c05364e1eaefa118f39b9f1dd
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-27 23:19:29 -0700

    Code is now loaded from a zip archive potentially containing multiple files

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use spool_vm::Config;
 use std::path::PathBuf;
 use crate::instruction::Instruction::*;
 use crate::instance::{Instance, Instance::*, Function};
-use std::intrinsics::transmute;
 use crate::instruction::{Chunk, Instruction, Bytecode};
 use std::rc::Rc;
 use std::collections::HashSet;
@@ -14,8 +13,11 @@ use crate::string_pool::StringPool;
 use std::cell::RefCell;
 use crate::vm::VM;
 use crate::instance::Function::Native;
-use std::io::Error;
+use std::io::{Error, Read};
 use crate::_type::TypeRegistry;
+use std::fs::File;
+use zip::ZipArchive;
+use zip::result::ZipError;
 
 mod vm;
 mod instruction;
@@ -27,12 +29,10 @@ mod _type;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let bytes = load_bytecode("test.sbc");
-    // print_bytes(&bytes, 30, 52);
     let mut string_pool = StringPool::new();
+    let bytecode = load_bytecode_zip("test.zip", &mut string_pool);
     let mut type_registry = TypeRegistry::new(&mut string_pool);
-    let mut bytecode_vec = Bytecode::from_bytes(bytes, &mut string_pool);
-    let mut bytecode_iter = bytecode_vec.iter();
+    let mut bytecode_iter = bytecode.iter();
     let mut main: Option<Rc<Chunk>> = None;
 
     loop {
@@ -48,8 +48,9 @@ fn main() {
     }
 
     let mut vm = VM::new(string_pool, type_registry);
+    println!("{:?}", bytecode);
     match main {
-        None => panic!(),
+        None => panic!("Main function not found in loaded bytecode."),
         Some(chunk) => vm.run(chunk),
     }
 }
@@ -74,4 +75,43 @@ fn print_bytes(bytes: &Vec<u8>, from: usize, to: usize) {
         index += 1
     });
     println!("]")
+}
+
+fn load_bytecode_zip(filename: &str, string_pool: &mut StringPool) -> Vec<Bytecode> {
+    let result = std::fs::File::open(filename);
+    let mut bytecode_vec: Vec<Bytecode> = vec![];
+
+    match result {
+        Ok(file) => {
+            match ZipArchive::new(file) {
+                Ok(mut archive) => {
+                    for i in 0..archive.len() {
+                        let mut file = archive.by_index(i).unwrap();
+                        let mut slice: &[u8] = &[0u8; 65535];
+                        let mut buf: Vec<u8> = Vec::from(slice);
+
+                        match file.read(&mut buf) {
+                            Ok(count) => buf.truncate(count),
+                            Err(err) => println!("Error occurred when reading zip file: {}", err),
+                        }
+
+                        let vec = Bytecode::from_bytes(buf, string_pool);
+                        for bytecode in vec {
+                            bytecode_vec.push(bytecode)
+                        }
+                    }
+                },
+                Err(error) => {
+                    println!("Error occurred when reading zip file: {}", error);
+                    panic!()
+                },
+            }
+        },
+        Err(error) => {
+            println!("Error occurred when loading zip file: {}", error);
+            panic!()
+        },
+    };
+
+    return bytecode_vec
 }
